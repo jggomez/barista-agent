d5df1812d3a841694f5bd8ec3402741fe29731e5
jggomez <juan.gomez01@gmail.com>
2025-10-30 15:22:03 -0600

    feat(all project):Pipeline was deployed to GCP

--- pipeline-data-ingestion-menu/src/ingestion_menu.py
+++ pipeline-data-ingestion-menu/src/ingestion_menu.py
@@ -1,13 +1,26 @@
+import logging
+
+import google.cloud.logging
+from cloudevents.http import from_http
 from dotenv import load_dotenv
+from flask import Flask
+from flask import request
 from google import genai
 from google.cloud import firestore
+from google.cloud import storage
 from google.cloud.firestore_v1.vector import Vector
 from google.genai import types
 from langchain_community.document_loaders import TextLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
+
+client = google.cloud.logging.Client()
+client.setup_logging()
+
 load_dotenv()
+
+app = Flask(__name__)
 
 # Constants
 MENU_FIRESTORE_COLLECTION = "menu"
@@ -15,7 +28,7 @@ DATABASE_NAME = "embeddings"
 GEMINI_MODEL_EMBEDDING = "gemini-embedding-001"
 
 
-def chunking(file_path: str) -> list:
+def chunking(file_path: str) -> list[Document]:
     loader = TextLoader(file_path)
     documents = loader.load()
 
@@ -28,7 +41,7 @@ def chunking(file_path: str) -> list:
 
 
 def create_embeddings(chunk: Document) -> tuple:
-    print("creating embeddings")
+    logging.info("creating embeddings")
     try:
         text_content = f"{chunk.metadata.get('MenuCategory')} - {chunk.page_content}"
 
@@ -44,12 +57,12 @@ def create_embeddings(chunk: Document) -> tuple:
 
         return result, text_content
     except Exception as e:
-        print(f"An error occurred in create_embeddings: {e}")
+        logging.error(f"An error occurred in create_embeddings: {e}")
         raise e
 
 
-def save_chunks_embeddings(chunks: list) -> None:
-    print("embeddings created and saved to Firestore")
+def save_chunks_embeddings(chunks: list[Document]) -> None:
+    logging.info("embeddings created and saved to Firestore")
     db = firestore.Client(database=DATABASE_NAME)
     try:
         for chunk in chunks:
@@ -65,8 +78,8 @@ def save_chunks_embeddings(chunks: list) -> None:
             )
 
     except Exception as e:
-        print(f"An error occurred in save_chunks_embeddings: {e}")
-        return
+        logging.error(f"An error occurred in save_chunks_embeddings: {e}")
+        raise e
 
 
 # create a function that receive a path file and chunking a file using langchain, create embeddings using gemini and save the embedddings in firestore
@@ -83,6 +96,29 @@ def ingestion_menu(file_path: str) -> None:
     save_chunks_embeddings(chunking_menu)
 
 
+@app.route("/pipeline", methods=["POST"])
+def index():
+    logging.info("A new file has been uploaded to the Cloud Storage bucket")
+    event = from_http(request.headers, request.data)
+    event_data = event.data
+
+    bucket = event_data.get("bucket")
+    file_name = event_data.get("name")
+    file_path = f"/tmp/{file_name}"
+    logging.info(f"Bucket: {bucket}, File name: {file_name}, File path: {file_path}")
+
+    storage_client = storage.Client()
+    bucket_gcs = storage_client.bucket(bucket)
+    blob = bucket_gcs.blob(file_name)
+    blob.download_to_filename(file_path)
+    logging.info(f"File downloaded to {file_path}")
+
+    ingestion_menu(file_path)
+
+    return (f"Detected change in Cloud Storage bucket: {bucket}", 200)
+
+
 if __name__ == "__main__":
-    menu_file = "resources/menu.md"
-    ingestion_menu(menu_file)
+    # menu_file = "resources/menu.md"
+    # ingestion_menu(menu_file)
+    app.run(debug=True, host="0.0.0.0", port=8080)
